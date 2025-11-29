8dd9c116aef914ca668ef09aaf699400df21c92f
Rahiq Rahman <rahiqrahman2001@gmail.com>
2025-11-29 22:42:02 +0600

    updated

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -9,20 +9,20 @@ const Timeline = () => {
   const scheduleData = [
     {
       title: "Md. Shakib Shahariar Junayed",
-      image: "src/assets/images/speaker1.png",
       position: "Secretary, IEEE NSU SB",
+      email: "Secretary, IEEE NSU SB",
       description: "Contact: 0123546798",
     },
     {
       title: "Md. Rakibul Islam",
-      image: "src/assets/images/speaker1.png",
       position: "Vice Chairperson, IEEE NSU SB",
+      email: "Vice Chairperson, IEEE NSU SB",
       description: "Contact: 0123546798",
     },
     {
       title: "Md. Nashrat",
-      image: "src/assets/images/speaker1.png",
       position: "Chairperson, IEEE NSU SB",
+      email: "Chairperson, IEEE NSU SB",
       description: "Contact: 0123546798",
     },
   ];
@@ -39,12 +39,6 @@ const Timeline = () => {
         <div className="flex max-w-[1050px] mx-auto flex-wrap justify-between md:mt-15 px-4 mt-10 md:pb-30 pb-20 z-10 relative ">
           {scheduleData.map((data, index) => (
             <div key={index} className="flex w-[500px] items-center md:flex-row flex-col  mt-10 bg-white p-4 md:rounded-[180px_100px_180px_100px] rounded-[80px_10px_80px_10px] border-l-1 border-b-2 border-r-0  border-ieee-blue shadow-md overflow-hidden max-md:border-t-1 max-md:border-r-1">
-              {/* Timeline Column suru */}
-              <img
-                src={data.image}
-                alt={data.name}
-                className="md:w-[150px] md:h-[150px] max-md:mb-5 w-50 h-50 object-cover rounded-full md:mr-6 md:rounded-[180px_100px_180px_100px]"
-              />
               {/* Content Column */}
               <div>
                 <h3 className="text-xl font-bold text-[#002855] mb-1">
