732b45cfb2b22be03bb1cdbc339a02242a9ffbd4
shohan12727 <aishohan001@gmail.com>
2025-12-04 01:21:25 +0600

    Enhance Ambassadors component with improved styling and hover effects

--- src/Ambassadors.jsx
+++ src/Ambassadors.jsx
@@ -35,17 +35,22 @@ const Ambassadors = () => {
       <div className="grid-bg">
         <SectionHeadingBG />
         <div className="max-w-5xl md:mx-auto mt-10 md:mt-15 z-10 relative">
-          <h2 className='text-white text-center md:text-left text-[24px] md:text-[30px] font-bold'>
+          <h2 className="text-white text-center md:text-left text-[24px] md:text-[30px] font-bold">
             SPAC 2025 Ambassadors
           </h2>
         </div>
         <div className="grid gap-16 grid-cols-1 md:grid-cols-3 max-w-[1020px] mx-auto md:mt-41 mt-25 md:pb-30 pb-20">
           {ambassadors.map((ambassador, index) => (
             <>
-              <div
+              {/* <div
                 key={index}
                 className="w-[90%] mx-auto md:w-[295px] h-[360px] overflow-hidden shadow-[0_8px_10px_0_rgba(0,0,0,0.3)]
  rounded-[7px] relative z-20"
+              > */}
+              <div
+                key={index}
+                className="w-[90%] mx-auto md:w-[295px] h-[360px] overflow-hidden shadow-[0_8px_10px_0_rgba(0,0,0,0.3)]
+rounded-[7px] relative z-20 transition-all duration-300 ease-out hover:transform hover:-translate-y-2 hover:shadow-[0_20px_30px_0_rgba(59,130,246,0.5)]"
               >
                 <img
                   src={ambassador.image}
